4a28b755b095c51aba388a86b210d991e505e0d4
rezaKeshtkarzadeh <129211850+rezaKeshtkarzadeh@users.noreply.github.com>
2024-05-29 15:57:17 +0330

    App initialization commit.

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    StyleSheet, Text, View
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+const App = () => {
+    return (
+        <View>
+            <StatusBar backgroundColor='#fff' animated />
+            
+        </View>
+    )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
